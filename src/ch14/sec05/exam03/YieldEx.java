341029a6fe133515760e0c6c70ca54235f754451
onwardjake <onwardjake@gmail.com>
2025-09-17 17:24:29 +0900

    주석 추가

--- src/ch14/sec05/exam03/YieldEx.java
+++ src/ch14/sec05/exam03/YieldEx.java
@@ -7,11 +7,13 @@ public class YieldEx {
         t1.start();
         t2.start();
 
+        // 200ms 후에 t1.work를 false로 바꿔서 다른 스레드에 양보한다.
         try{
             Thread.sleep(200);
         } catch (InterruptedException e) {}
         t1.work = false;
 
+        // 500ms 후에 t1.work를 true로 바꿔서 t1, t2가 번갈아가면서 수행되도록 한다.
         try{
             Thread.sleep(500);
         } catch (InterruptedException e) {}
